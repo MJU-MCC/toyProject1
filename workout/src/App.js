8eff90e047db53ac6bbaf5ff89da5568137638bd
Keon Hyouk <khkoh5932@naver.com>
2024-01-15 02:53:28 +0900

    route link

--- workout/src/App.js
+++ workout/src/App.js
@@ -5,21 +5,16 @@ import {BrowserRouter,Route,Routes}from "react-router-dom"
 
 function App() {
   return (
-    <div className="App">
+    <BrowserRouter>
+      <div className="App">
 
-        <Navbar/>
-        <Routes>
-          <Route path='/'>
-            <div className="box">
-                <div className="main-contents">
-                    homepage
-                    info
-                </div>
-                <Sidebar/>
-            </div>
-          </Route>
-        </Routes>
-    </div>
+          <Navbar/>
+          <Routes>
+            <Route path='/' element={<Sidebar/>}></Route>
+            <Route path='/signup'></Route>
+          </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- workout/src/component/sidebar.js
+++ workout/src/component/sidebar.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import styles from "./sidebar.module.css"
 import Login from "./login";
+import { Link } from "react-router-dom";
 
 function Sidebar(){
 
@@ -10,8 +11,8 @@ function Sidebar(){
         <div className={styles.sidebar}>
             <div className={styles.buttons}>
                 {login===true?<Login/>:null}
-                <button className={styles.signin} type="submit" onClick={()=>{setLogin(!login)}}>sign in</button>
-                <button className={styles.signup} type="submit">sign up</button>
+                <button className={styles.signin} onClick={()=>{setLogin(!login)}}>sign in</button>
+                <Link to="/signup" className={styles.signup}>sign up</Link>
             </div>
         </div>
         
